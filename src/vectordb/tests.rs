c04f9b13d528a0cdf1df79fe9eeb01119c4288f9
Rupesh Raghavan <rupesh_raghavan@apple.com>
2024-12-18 22:01:04 -0800

    Add docker-compose setup for PostgreSQL and enhance pg_client tests

--- src/vectordb/tests.rs
+++ src/vectordb/tests.rs
@@ -1,12 +1,15 @@
 #[cfg(test)]
-mod tests {
-    use super::super::pg_vector::pg_client;
-    use crate::app::config::VectorDbConfig;
-    use std::io::{self, Write};
-    use std::process::Command;
+use super::pg_vector::pg_client;
+use crate::app::config::VectorDbConfig;
 
-    fn setup_docker() -> io::Result<()> {
-        // Create a docker-compose.yml string
+// run docker compose up -d to start the database
+use std::io::{self, Write};
+use std::process::Command;
+
+#[test]
+fn test_setup_docker() -> io::Result<()> {
+    // Create a docker-compose.yml string
+    let docker_compose_yml = r#"
         let docker_compose_yml = r#"
 version: '3.8'
 
@@ -27,129 +30,119 @@ volumes:
   pg_data:
 "#;
 
-        // Write the docker-compose.yml to a file
-        let file_name = "docker-compose-test.yml";
-        let mut file = std::fs::File::create(file_name)?;
-        write!(file, "{}", docker_compose_yml)?;
+    // Write the docker-compose.yml to a file
+    let file_name = "docker-compose-test.yml";
+    let mut file = std::fs::File::create(file_name)?;
+    file.write_all(docker_compose_yml.as_bytes())?;
 
-        // Execute docker-compose up
-        let output = Command::new("docker-compose")
-            .arg("up")
-            .arg("-d")
-            .arg("-f")
-            .arg(file_name)
-            .output()?;
+    // Execute docker-compose up
+    let output = Command::new("docker-compose")
+        .arg("up")
+        .arg("-d")
+        .arg("-f")
+        .arg(file_name)
+        .output()?;
 
-        // Check if the command was successful
-        if output.status.success() {
-            println!("Docker compose set up successfully.");
-        } else {
-            let stderr = String::from_utf8_lossy(&output.stderr);
-            eprintln!("Error running docker-compose: {}", stderr);
-        }
-
-        Ok(())
+    // Check if the command was successful
+    if output.status.success() {
+        println!("Docker compose set up successfully.");
+    } else {
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        eprintln!("Error running docker-compose: {}", stderr);
     }
 
-    fn teardown_docker() -> io::Result<()> {
-        // Execute docker-compose down
-        let output = Command::new("docker-compose").arg("down").output()?;
+    Ok(())
+}
 
-        // Check if the command was successful
-        if output.status.success() {
-            println!("Docker compose stopped successfully.");
-        } else {
-            let stderr = String::from_utf8_lossy(&output.stderr);
-            eprintln!("Error running docker-compose: {}", stderr);
-        }
+#[test]
+fn test_pg_client_success() {
+    // Arrange
+    let db_config = VectorDbConfig {
+        host: String::from("0.0.0.0"),
+        port: 5432,
+        user: String::from("test_user"),
+        dbname: String::from("test_db"),
+        timeout: 5,
+    };
 
-        // Delete the docker-compose-test.yml file
-        std::fs::remove_file("docker-compose-test.yml")?;
+    // Act
+    let result = pg_client(&db_config);
 
-        Ok(())
+    // Assert
+    assert!(result.is_ok());
+}
+
+#[test]
+fn test_pg_client_invalid_host() {
+    // Arrange
+    let db_config = VectorDbConfig {
+        host: String::from("invalid_host"),
+        port: 5432,
+        user: String::from("test_user"),
+        dbname: String::from("test_db"),
+        timeout: 5,
+    };
+
+    // Act
+    let result = pg_client(&db_config);
+
+    // Assert
+    assert!(result.is_err());
+    // Uncomment the line below to see the error message
+    // println!("{:?}", result.err());
+}
+
+#[test]
+fn test_pg_client_timeout() {
+    // Arrange
+    let db_config = VectorDbConfig {
+        host: String::from("localhost"),
+        port: 5432,
+        user: String::from("test_user"),
+        dbname: String::from("test_db"),
+        timeout: 0, // Timeout set to 0 to simulate an immediate timeout
+    };
+
+    // Act
+    let result = pg_client(&db_config);
+
+    // Assert
+    assert!(result.is_err());
+}
+
+#[test]
+fn test_pg_client_null_config() {
+    // Arrange
+    let db_config: VectorDbConfig = VectorDbConfig {
+        host: String::from(""),
+        port: 0,
+        user: String::from(""),
+        dbname: String::from(""),
+        timeout: 0,
+    };
+
+    // Act
+    let result = pg_client(&db_config);
+
+    // Assert
+    assert!(result.is_err());
+}
+
+#[test]
+fn stop_docker() -> io::Result<()> {
+    // Execute docker-compose down
+    let output = Command::new("docker-compose").arg("down").output()?;
+
+    // Check if the command was successful
+    if output.status.success() {
+        println!("Docker compose stopped successfully.");
+    } else {
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        eprintln!("Error running docker-compose: {}", stderr);
     }
 
-    #[test]
-    fn docker_setup_and_pg_client_success() {
-        // Setup Docker
-        setup_docker().expect("Failed to set up Docker");
+    // delete the docker-compose-test.yml file
+    std::fs::remove_file("docker-compose-test.yml")?;
 
-        // Run the pg_client success test
-        let db_config = VectorDbConfig {
-            host: String::from("localhost"),
-            port: 5432,
-            user: String::from("test_user"),
-            dbname: String::from("test_db"),
-            timeout: 5,
-        };
-
-        let result = pg_client(&db_config);
-        assert!(result.is_ok());
-
-        // Teardown Docker
-        teardown_docker().expect("Failed to tear down Docker");
-    }
-
-    #[test]
-    fn docker_setup_and_pg_client_invalid_host() {
-        // Setup Docker
-        setup_docker().expect("Failed to set up Docker");
-
-        // Run the pg_client invalid host test
-        let db_config = VectorDbConfig {
-            host: String::from("invalid_host"),
-            port: 5432,
-            user: String::from("test_user"),
-            dbname: String::from("test_db"),
-            timeout: 5,
-        };
-
-        let result = pg_client(&db_config);
-        assert!(result.is_err());
-
-        // Teardown Docker
-        teardown_docker().expect("Failed to tear down Docker");
-    }
-
-    #[test]
-    fn docker_setup_and_pg_client_timeout() {
-        // Setup Docker
-        setup_docker().expect("Failed to set up Docker");
-
-        // Run the pg_client timeout test
-        let db_config = VectorDbConfig {
-            host: String::from("localhost"),
-            port: 5432,
-            user: String::from("test_user"),
-            dbname: String::from("test_db"),
-            timeout: 0,
-        };
-
-        let result = pg_client(&db_config);
-        assert!(result.is_err());
-
-        // Teardown Docker
-        teardown_docker().expect("Failed to tear down Docker");
-    }
-
-    #[test]
-    fn docker_setup_and_pg_client_null_config() {
-        // Setup Docker
-        setup_docker().expect("Failed to set up Docker");
-
-        // Run the pg_client null config test
-        let db_config = VectorDbConfig {
-            host: String::from(""),
-            port: 0,
-            user: String::from(""),
-            dbname: String::from(""),
-            timeout: 0,
-        };
-
-        let result = pg_client(&db_config);
-        assert!(result.is_err());
-
-        // Teardown Docker
-        teardown_docker().expect("Failed to tear down Docker");
-    }
+    Ok(())
 }
