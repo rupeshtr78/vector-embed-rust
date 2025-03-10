815775851db4bf7c752b810e8f2976dcb67150e7
rupeshtr78 <rupeshtr78@gmail.com>
2025-03-09 19:26:42 -0700

    refactor: streamline query handling with new functions

--- src/lancevectordb/query.rs
+++ src/lancevectordb/query.rs
@@ -10,8 +10,9 @@ use lancedb::arrow::SendableRecordBatchStream;
 use lancedb::query::ExecutableQuery;
 use lancedb::query::IntoQueryVector;
 use lancedb::query::QueryBase;
-use lancedb::Connection;
+use lancedb::{Connection, Table};
 use log::{debug, error, info};
+use arrow_array::RecordBatch;
 
 /// Run the query to get the nearest embeddings
 /// Arguments:
@@ -43,6 +44,7 @@ pub async fn run_query(
 
     let url = format!("{}/{}", CHAT_API_URL, "api/embed");
 
+    // create embedder request for query
     let query_request_arc =
         EmbedRequest::NewArcEmbedRequest(&embed_model, input_list, &"".to_string(), None);
     let query_response = embedder::fetch_embedding(&url, &query_request_arc, http_client)
@@ -51,6 +53,7 @@ pub async fn run_query(
 
     let query_vector = query_response.embeddings[0].clone();
 
+    // query the vector table
     let content = query_vector_table(db, vector_table, query_vector, whole_query)
         .await
         .context("Failed to query table")?;
@@ -78,45 +81,22 @@ pub async fn query_vector_table(
     let batches;
 
     if !whole_query {
-        let stream: SendableRecordBatchStream = table
-            .query()
-            .nearest_to(query_vector) // Find the nearest vectors to the query vector
-            .context("Failed to select nearest vector")?
-            // .distance_range(lower_bound, upper_bound) // bug in DataFusion library
-            .distance_type(lancedb::DistanceType::Cosine)
-            .refine_factor(10)
-            .limit(20)
-            .nprobes(40) // default is 20
-            .postfilter()
-            // .only_if("_distance > 0.3 AND _distance < 1")
-            .select(lancedb::query::Select::Columns(vec![
-                "_distance".to_string(),
-                "chunk_number".to_string(),
-                "metadata".to_string(),
-                "content".to_string(),
-            ]))
-            .execute()
-            .await
-            .context("Failed to execute query and fetch records")?;
+        let stream = query_nearest_vector(query_vector, &table).await?;
 
         batches = stream.collect::<Vec<_>>().await;
     } else {
-        let stream = table
-            .query()
-            .only_if("content IS NOT NULL".to_string())
-            .select(lancedb::query::Select::Columns(vec![
-                "id".to_string(),
-                "metadata".to_string(),
-                "content".to_string(),
-            ]))
-            .limit(1000)
-            .execute()
-            .await
-            .context("Failed to execute whole query and fetch records")?;
+        let stream = query_all_content(table).await?;
 
         batches = stream.collect::<Vec<_>>().await;
     }
+    
+    debug!("Number of batches from query: {}", batches.len());
+    let content = get_content_from_batch(batches).context("Failed to get content from record batch")?;
 
+    Ok(content)
+}
+
+fn get_content_from_batch(batches: Vec<lancedb::error::Result<RecordBatch>>) -> Result<Vec<String>> {
     for batch in batches {
         let batch: arrow_array::RecordBatch = batch.context("Failed to get RecordBatch")?;
         // println!("Batch: {:?}", batch);
@@ -139,8 +119,49 @@ pub async fn query_vector_table(
             }
         }
     }
-
+    
+    error!("No content found in the batch");
     Ok(Vec::new())
+}
+
+async fn query_all_content(table: Table) -> Result<SendableRecordBatchStream> {
+    let stream = table
+        .query()
+        .only_if("content IS NOT NULL".to_string())
+        .select(lancedb::query::Select::Columns(vec![
+            "id".to_string(),
+            "metadata".to_string(),
+            "content".to_string(),
+        ]))
+        .limit(1000)
+        .execute()
+        .await
+        .context("Failed to execute whole query and fetch records")?;
+    Ok(stream)
+}
+
+async fn query_nearest_vector(query_vector: impl IntoQueryVector + Sized, table: &Table) -> Result<SendableRecordBatchStream> {
+    let stream: SendableRecordBatchStream = table
+        .query()
+        .nearest_to(query_vector) // Find the nearest vectors to the query vector
+        .context("Failed to select nearest vector")?
+        // .distance_range(lower_bound, upper_bound) // bug in DataFusion library
+        .distance_type(lancedb::DistanceType::Cosine)
+        .refine_factor(10)
+        .limit(20)
+        .nprobes(40) // default is 20
+        .postfilter()
+        // .only_if("_distance > 0.3 AND _distance < 1")
+        .select(lancedb::query::Select::Columns(vec![
+            "_distance".to_string(),
+            "chunk_number".to_string(),
+            "metadata".to_string(),
+            "content".to_string(),
+        ]))
+        .execute()
+        .await
+        .context("Failed to execute query and fetch records")?;
+    Ok(stream)
 }
 
 pub fn get_content(batch: &StringArray) -> Result<Vec<String>> {
